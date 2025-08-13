188e88bb95e366feabd67eb280bc475be0dbb0da
IT - Bowie Lam <tylam@shoalter.com>
2025-08-13 10:42:52 +0800

    Added neo-tree, keybindings for lsp

--- lua/config/lsp.lua
+++ lua/config/lsp.lua
@@ -8,11 +8,17 @@ vim.api.nvim_create_autocmd('LspAttach', {
                 vim.lsp.completion.get()
             end)
         end
-    end,
-})
 
-vim.diagnostic.config({
-    virtual_lines = {
-        current_line = true,
-    },
+        local keymap_opt = { buffer = ev.buf, nowait = true, noremap = true, silent = true }
+        vim.keymap.set("n", "gd", vim.lsp.buf.definition, keymap_opt)
+        vim.keymap.set("n", "gD", vim.lsp.buf.declaration, keymap_opt)
+        vim.keymap.set("n", "gu", vim.lsp.buf.references, keymap_opt)
+        vim.keymap.set("n", "gi", vim.lsp.buf.implementation, keymap_opt)
+        vim.keymap.set("n", "gh", vim.diagnostic.open_float, keymap_opt)
+        vim.keymap.set("n", "gpd", vim.diagnostic.goto_prev, keymap_opt)
+        vim.keymap.set("n", "gnd", vim.diagnostic.goto_next, keymap_opt)
+        vim.keymap.set("n", "<leader>f", vim.lsp.buf.format, keymap_opt)
+        vim.keymap.set("n", "<leader>r", vim.lsp.buf.rename, keymap_opt)
+        vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, keymap_opt)
+    end,
 })
--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -8,5 +8,10 @@ return {
       "nvim-tree/nvim-web-devicons", -- optional, but recommended
     },
     lazy = false, -- neo-tree will lazily load itself
+    config = function()
+        local keymap_opts = { noremap = true, silent = true }
+        vim.keymap.set("n", "<leader>e", "<Cmd>Neotree toggle<CR>", keymap_opts)
+        vim.keymap.set("n", "<leader>o", "<Cmd>Neotree reveal<CR>", keymap_opts)
+    end
   }
 }
