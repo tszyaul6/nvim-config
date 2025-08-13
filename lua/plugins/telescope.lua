a97878d98a30b3143fa8bd5272c5a73154dd483a
IT - Bowie Lam <tylam@shoalter.com>
2025-08-13 17:28:34 +0800

    telescope use shorthand file name

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -4,7 +4,12 @@ return {
     dependencies = { 'nvim-lua/plenary.nvim' },
     config = function()
         local builtin = require('telescope.builtin')
-        vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
+
+        local find_files_truncate = function()
+            builtin.find_files({ path_display = { "truncate" } })
+        end
+
+        vim.keymap.set('n', '<leader>ff', find_files_truncate, { desc = 'Telescope find files', })
         vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
         vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
         vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
