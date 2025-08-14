8c34d25579b35fc18894d12124b3423274daeb9a
IT - Bowie Lam <tylam@shoalter.com>
2025-08-14 11:05:45 +0800

    Fixed gitsigns contradiction with lsp warnings

--- lua/plugin/gitsigns-fugitive.lua
+++ lua/plugin/gitsigns-fugitive.lua
@@ -1,7 +1,11 @@
 return {
     {
         "lewis6991/gitsigns.nvim",
-        config = function()
+        opts = {
+            sign_priority = 100
+        },
+        config = function(_, opts)
+            require("gitsigns").setup(opts)
             vim.keymap.set("n", "<leader>gp", ":Gitsigns preview_hunk<CR>")
             vim.keymap.set("n", "gph", ":Gitsigns prev_hunk<CR>")
             vim.keymap.set("n", "gnh", ":Gitsigns next_hunk<CR>")
