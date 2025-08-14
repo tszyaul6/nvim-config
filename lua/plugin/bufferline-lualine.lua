1c94fff341693bcec19d08a3ca6feed604fa3273
IT - Bowie Lam <tylam@shoalter.com>
2025-08-14 13:32:45 +0800

    Added lualine support

--- lua/plugin/bufferline-lualine.lua
+++ lua/plugin/bufferline-lualine.lua
@@ -1,4 +1,13 @@
 return {
-    { 'akinsho/bufferline.nvim', version = "*", dependencies = 'nvim-tree/nvim-web-devicons', opts = {} }
-
+    {
+        'akinsho/bufferline.nvim',
+        version = "*",
+        dependencies = 'nvim-tree/nvim-web-devicons',
+        opts = {}
+    },
+    {
+        'nvim-lualine/lualine.nvim',
+        dependencies = { 'nvim-tree/nvim-web-devicons' },
+        opts = {}
+    }
 }
